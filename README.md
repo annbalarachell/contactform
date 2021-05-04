05bd51dc2821a3d84687c03a77a0bfe589ebeb3d
annbalarachell <annbalarachell@gmail.com>
2021-05-04 18:51:12 +0530

    Contact Form

---
+++ README.md
@@ -0,0 +1,14 @@
+# Vue + Tailwind Contact Form Test Task
+
+## Project setup
+```
+yarn install
+yarn run serve
+```
+
+Or
+
+```
+npm install
+npm run serve
+```
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div class="text-center">
+      <h1 class="text-3xl font-bold">Create user on Techines</h1>
+      <!-- <p class="text-xl text-gray-600 tracking-wider uppercase font-bold">Interactive component examples</p> -->
+      <form class="flex-wrap">
+        <label>Name</label><br/>
+        <input type="text" class=""><br/>
+        <label>Name</label><br/>
+        <input type="text" class=""><br/>
+        <label>Name</label><br/>
+        <input type="text" class=""><br/>
+        <label>Name</label><br/>
+        <input type="text" class=""><br/>
+        <label>Name</label><br/>
+        <input type="text" class=""><br/>
+        <label>Already a user ? <a href="#">Login</a></label><br/>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create user</button>
+      </form>
+    </div>
+  </div>
+</template>
